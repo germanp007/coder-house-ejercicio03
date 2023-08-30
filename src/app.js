b08c2e4121fc35725ba751ce04559f7551ee0e32
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-08-30 12:13:37 -0300

    Create app.js
    Creando servidor con Express, y agregando la class ProductManager hecho en las clases anteriores.

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { ProductManager } from './ProductManager.mjs';
+
+const server = express();
+
+const productList = new ProductManager('./productList.json')
+
+server.get('/products', async (req,res)=>{
+    try {
+        const limit = +req.query.limit; // Convierte el valor a número usando +
+        const list = await productList.getProducts();
+        
+        if (!isNaN(limit)) { // Verificamos si es un numero, devolvera true
+            let result;
+            if(limit > list.length){
+                res.send("Error, Limite excedido")
+            }else{
+                result = list.slice(0, limit);
+                res.send(result)
+            }
+
+        } else {
+            res.send(list);
+        }
+    } catch (error) {
+        res.send(error);
+    }
+    
+});
+server.get('/products/:productId', async (req,res)=>{
+    try {
+        const ID = +req.params.productId; // Convierte el valor a número usando +
+        const list = await productList.getProducts();
+        const result = list.find(ele => ele.id === ID);
+        if(result){
+
+            res.send(result)
+        } else{
+            res.send(`<h1>Producto No encontrado</h1>`)
+        }
+     
+    } catch (error) {
+        res.send(error);
+    }
+    
+})
+
+server.listen(8080, ()=>{
+    console.log('servidor en el puerto 8080')
+});
